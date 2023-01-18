4a141e12dfac04163f41078fa0e76f658ac35365
Nick Campos <nickreyescampos@gmail.com>
2023-01-18 00:44:30 -0800

    lists

---
+++ Lists/Lists.py
@@ -0,0 +1,6 @@
+newList = [1, 2, 3, 4, 5]       # initialize new list
+print(newList)                  # prints out the list
+
+for num in range(1, len(newList) + 1): # readds every element in the list
+    newList.append(num)
+print(newList)                  # print out the list
